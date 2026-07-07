e59e24b74b7aa73be15fa0645e1321421cc1f0f2
TurboTurboks <teotuta3@gmail.com>
2026-07-07 11:44:25 +0200

    Dodati operate funkciju funkcija poziva jednu od racunksih operacija po odabiru

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+let numberOne = 0;
+let numberTwo = 0;
+let operator = "+"; 
+
+
+
+function operate(operator, numberOne, numberTwo) {
+    switch (operator) {
+        case "+":
+            return add(numberOne, numberTwo)
+        case "-":
+            return subtract(numberOne, numberTwo)
+        case "*":
+            return multiply(numberOne, numberTwo)
+        case "/":
+            return divide(numberOne, numberTwo)
+    }
+}
+
+
 function add (a, b) {
     return a + b
 }
