07f83c6fd4789149a5d0136f074f8b0a9a6d5386
TurboTurboks <teotuta3@gmail.com>
2026-07-08 16:47:56 +0200

    Zavrsiti calculator 1.0 Zavrsiti sve sto je trazeno od mene za project calculator i bonus decimale i brisanja

--- script.js
+++ script.js
@@ -2,25 +2,27 @@ let numberOne = "";
 let numberTwo = "";
 let operator = "";
 let numberSwitch = true;
-let result = 0;
+let result = "";
 
 const operators = ["+", "-", "*", "/"]
 
 
 let display = document.querySelector(".display")
 
-buttons = document.querySelectorAll("button")
+const buttons = document.querySelectorAll("button")
 
 buttons.forEach((button) => {
     button.addEventListener('click', (event) => {
 
         if (Number(button.textContent) >= 0) {
             if (result == display.textContent) clear();
-                
+
 
             display.textContent += button.textContent;
             if (numberSwitch) numberOne += button.textContent;
             if (!numberSwitch) numberTwo += button.textContent;
+
+
 
         }
         if (operators.includes(button.textContent)) {
@@ -30,43 +32,75 @@ buttons.forEach((button) => {
                 numberSwitch = false;
             }
             else {
-                result = operate(operator, Number(numberOne), Number(numberTwo))
-                numberOne = result
-                display.textContent = numberOne;
-                numberTwo = "";
-                operator = button.textContent;
-                display.textContent += button.textContent;
+                if (numberTwo != "") {
+                    result = operate(operator, Number(numberOne), Number(numberTwo))
+                    numberOne = result
+                    display.textContent = numberOne;
+                    numberTwo = "";
+                    operator = button.textContent;
+                    display.textContent += button.textContent;
+                }
+                else {
+                    operator = button.textContent;
+                    display.textContent = display.textContent.slice(0, -1)
+                    display.textContent += button.textContent;
+                }
             }
 
         }
+
         if (button.textContent == "=") {
-            result = operate(operator, Number(numberOne), Number(numberTwo))
-            display.textContent = result;
+            if (numberTwo != "") {
+                result = operate(operator, Number(numberOne), Number(numberTwo))
+                numberOne = result;
+                numberTwo = "";
+                operator = "";
+                numberSwitch = true;
+                display.textContent = result;
+            }
         }
         if (button.textContent == "C") {
             clear();
         }
         if (button.textContent == "Del") {
-            if (result == display.textContent) return;
-            if (Number(display.textContent.at(-1) >= 0 || display.textContent.at(-1) == ".")) {
+            if (result == Number(display.textContent)) return;
+            if (Number(display.textContent.at(-1)) >= 0 || display.textContent.at(-1) == ".") {
                 if (numberSwitch) numberOne = numberOne.slice(0, -1);
                 if (!numberSwitch) numberTwo = numberTwo.slice(0, -1);
                 display.textContent = display.textContent.slice(0, -1)
             }
+            if (operators.includes(display.textContent.at(-1))) {
+                operator = "";
+                display.textContent = display.textContent.slice(0, -1)
+            }
         }
         if (button.textContent == ".") {
-            if (numberSwitch) numberOne += button.textContent;
-            if (!numberSwitch) numberTwo += button.textContent;
-            display.textContent += button.textContent;
-        }
 
+            if (numberSwitch && !numberOne.includes(".")) { //numberOne prvi
+                console.log(numberOne)
+                if (numberOne == "") {
+                    numberOne += "0";
+                    display.textContent += "0";
+                }
+                numberOne += button.textContent;
+                display.textContent += button.textContent;
+
+            }
+
+
+            if (!numberSwitch && !numberTwo.includes(".")) { //numberTwo drugi
+                if (numberTwo == "") {
+                    numberTwo += "0";
+                    display.textContent += "0";
+                }
+                numberTwo += button.textContent;
+                display.textContent += button.textContent;
+            }
+
+
+        }
     })
 })
-
-
-
-// da se ne mjenja kad spamam operacione znakove
-
 
 
 
@@ -77,6 +111,7 @@ function clear() {
     operator = "";
     numberSwitch = true;
     display.textContent = "";
+    result = "";
 }
 
 function operate(operator, numberOne, numberTwo) {
@@ -88,7 +123,7 @@ function operate(operator, numberOne, numberTwo) {
         case "*":
             return multiply(numberOne, numberTwo)
         case "/":
-            if (numberOne == 0 || numberTwo == 0) return "DONT USE ZERO :("
+            if (numberTwo == 0) return "DONT USE ZERO :("
             else return divide(numberOne, numberTwo)
     }
 }
